4e19abe4dbe9bf7064989c63b289d3b6f45fa8de
Alexey Fansky <afansky@gmail.com>
2012-05-27 09:03:37 +0300

    removed module

--- s-modules/trunk/s-addurl/src/main/java/com/seo/addurl/service/AddUrlService.java
+++ s-modules/trunk/s-addurl/src/main/java/com/seo/addurl/service/AddUrlService.java
@@ -1,7 +1,5 @@
 package com.seo.addurl.service;
 
-import com.seo.addurl.service.exception.AddUrlFailedException;
-
 public interface AddUrlService {
     void postUrl(String url) throws AddUrlFailedException;
 }
