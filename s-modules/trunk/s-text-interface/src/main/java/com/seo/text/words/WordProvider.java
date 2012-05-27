648d4b46a276b9bcc701bd32603ed065dbdeb869
Alexey Fansky <afansky@gmail.com>
2012-05-27 09:38:43 +0300

    removing text interface

--- s-modules/trunk/s-text-interface/src/main/java/com/seo/text/words/WordProvider.java
+++
@@ -1,6 +0,0 @@
-package com.seo.text.words;
-
-public interface WordProvider {
-    String getRandomWord(int minsize, int maxsize);
-    String getRandomWordWithDigits(int min, int max);    
-}
---
+++ s-modules/trunk/s-text/src/main/java/com/seo/text/generation/keyword/KeywordInserter.java
@@ -0,0 +1,5 @@
+package com.seo.text.generation.keyword;
+
+public interface KeywordInserter {
+    String insertKeyword(String text, String keyword);
+}
