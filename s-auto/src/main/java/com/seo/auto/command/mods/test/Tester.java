0be095f91540440eca5609beeccc1e79d1aa4a6e
Alexey.Fansky <8swrN7Em>
2012-10-10 14:05:01 +0300

    fixing dependencies

--- s-auto/src/main/java/com/seo/auto/command/mods/test/Tester.java
+++
@@ -1,5 +0,0 @@
-package com.seo.auto.command.mods.test;
-
-public interface Tester {
-    public void test(TestCommand testCommand);
-}
--- s-auto/src/main/java/com/seo/auto/parser/ProjectParser.java
+++
@@ -1,8 +0,0 @@
-package com.seo.auto.parser;
-
-import com.seo.auto.model.Project;
-import com.seo.auto.parser.exception.ConfigErrorException;
-
-public interface ProjectParser {
-    Project parseConfig(String config) throws ConfigErrorException;
-}
