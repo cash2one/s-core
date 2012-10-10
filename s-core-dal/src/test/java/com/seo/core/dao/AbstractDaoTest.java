0f825d1a8848a7a067ed1b410d88ba0057a56b01
Alexey.Fansky <8swrN7Em>
2012-10-10 17:17:28 +0300

    adding spring version dependency

--- s-core-dal/src/test/java/com/seo/core/dao/AbstractDaoTest.java
+++ s-core-dal/src/test/java/com/seo/core/dao/AbstractDaoTest.java
@@ -1,7 +1,24 @@
 package com.seo.core.dao;
 
 import com.seo.core.AbstractDalTest;
+import com.seo.core.model.BaseModel;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 public abstract class AbstractDaoTest extends AbstractDalTest {
+    protected BaseRepository<BaseModel, Long> repository;
 
+    public abstract BaseModel createModel();
+
+    @Test
+    public void testSave(){
+        BaseModel model = createModel();
+
+        model = repository.save(model);
+
+        BaseModel loaded = repository.findOne(model.getId());
+
+        assertEquals(model.getId(), loaded.getId());
+    }
 }
--- s-core-dal/src/test/java/com/seo/core/dao/AutoConfigRepositoryTest.java
+++ s-core-dal/src/test/java/com/seo/core/dao/AutoConfigRepositoryTest.java
@@ -1,4 +1,19 @@
 package com.seo.core.dao;
 
+import com.seo.core.model.AutoConfig;
+import com.seo.core.model.BaseModel;
+
+import javax.inject.Inject;
+
 public class AutoConfigRepositoryTest extends AbstractDaoTest {
+
+    @Inject
+    public void setRepository(AutoConfigRepository autoConfigRepository) {
+        repository = (BaseRepository) autoConfigRepository;
+    }
+
+    @Override
+    public BaseModel createModel() {
+        return new AutoConfig(null, "test auto config", "config");
+    }
 }
