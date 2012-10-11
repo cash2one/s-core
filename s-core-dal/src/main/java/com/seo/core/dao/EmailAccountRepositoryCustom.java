48e2dbdb221371ddff50007ff5dc67ddc463a31a
Alexey Fansky <afansky@gmail.com>
2012-10-11 10:30:51 +0300

    implementing email account dao

--- s-core-dal/src/main/java/com/seo/core/dao/EmailAccountRepositoryCustom.java
+++
@@ -1,9 +0,0 @@
-package com.seo.core.dao;
-
-import com.seo.core.model.account.EmailAccount;
-
-public interface EmailAccountRepositoryCustom {
-    EmailAccount getRandom();
-    EmailAccount getRandomByHost(String type);
-    EmailAccount getByEmail(String email);
-}
