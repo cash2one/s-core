80144bbb985f1d557318865ba0b5c3bae4f66e68
Alexey Fansky <afansky@gmail.com>
2012-10-12 00:36:23 +0300

    adding s-auto-api module

---
+++ s-auto-api/src/main/java/com/seo/auto/web/controller/AutoConfigController.java
@@ -0,0 +1,13 @@
+package com.seo.auto.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AutoConfigController {
+
+    @RequestMapping
+    public void addConfig() {
+
+    }
+}
