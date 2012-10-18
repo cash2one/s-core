4fcab9b6a649cdb742035b9babad5b370fc77056
Alexey Fansky <afansky@gmail.com>
2012-10-18 15:44:11 +0300

    fixing loggers

--- s-auto-api/src/main/java/com/seo/auto/web/model/enums/ResponseStatus.java
+++ s-auto-api/src/main/java/com/seo/auto/web/model/enums/ResponseStatus.java
@@ -1,7 +1,5 @@
 package com.seo.auto.web.model.enums;
 
-import org.omg.PortableInterceptor.SUCCESSFUL;
-
 public enum ResponseStatus {
     SUCCESS, FAILURE
 }
--- s-core/src/test/java/com/seo/auto/facade/ConfigFacadeTest.java
+++ s-core/src/test/java/com/seo/auto/facade/ConfigFacadeTest.java
@@ -15,13 +15,12 @@ public class ConfigFacadeTest extends AbstractCoreTest {
             "<project xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:noNamespaceSchemaLocation=\"../../src/main/resources/config.xml.xsd\">\n" +
             "    <commands>\n" +
             "        <save-image-command name=\"imagedata\" url=\"http://habrahabr.ru/core/captcha/\"/>\n" +
-            "        <anti-captcha-command name=\"captcha_response\" image=\"${imagedata}\" ext=\"png\" phrase=\"false\"/>\n" +
+//            "        <anti-captcha-command name=\"captcha_response\" image=\"${imagedata}\" ext=\"png\" phrase=\"false\"/>\n" +
             "        <load-useragent-command/>\n" +
             "    </commands>\n" +
             "</project>";
 
     @Test
-    @Ignore
     public void testRunConfig() {
         configFacade.processConfig(TEST_CONFIG);
     }
