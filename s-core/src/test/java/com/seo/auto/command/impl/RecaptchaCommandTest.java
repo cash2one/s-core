a74e11fed24177f9114f956a18b77977cb8b88af
Alexey Fansky <afansky@gmail.com>
2012-10-27 01:02:11 +0300

    unit testing recaptcha command

--- s-core/src/test/java/com/seo/auto/command/impl/RecaptchaCommandTest.java
+++ s-core/src/test/java/com/seo/auto/command/impl/RecaptchaCommandTest.java
@@ -7,12 +7,20 @@ import com.seo.auto.client.CommandClientImpl;
 import com.seo.auto.command.request.RecaptchaCommand;
 import com.seo.captcha.CaptchaService;
 import com.seo.provider.manager.ProviderManager;
+import com.seo.webclient.WebClient;
+import com.seo.webclient.model.EncodedImage;
+import com.seo.webclient.model.Request;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
 
 import javax.inject.Inject;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.when;
 
 public class RecaptchaCommandTest extends AbstractCoreTest {
 
@@ -21,6 +29,9 @@ public class RecaptchaCommandTest extends AbstractCoreTest {
 
     @Mock
     private CaptchaService captchaService;
+
+    @Mock
+    private WebClient webClient;
 
     @Before
     public void setUp() {
@@ -31,6 +42,10 @@ public class RecaptchaCommandTest extends AbstractCoreTest {
 
     @Test
     public void testRecaptchaCommand() throws Exception {
+        when(webClient.retrieveRecaptchaChallenge(any(Request.class))).thenReturn("testchallenge");
+        when(webClient.retrieveImage(any(Request.class))).thenReturn(new EncodedImage("testhash"));
+        when(captchaService.retrieve("testhash", "jpg", true)).thenReturn("testcode");
+
         RecaptchaCommand command = new RecaptchaCommand();
 
         command.setImage("testimage");
@@ -39,6 +54,13 @@ public class RecaptchaCommandTest extends AbstractCoreTest {
 
         command.initCommand(providerManager);
 
-        command.execute(CommandClientImpl.newInstance());
+        CommandClient commandClient = CommandClientImpl.newInstance();
+
+        InjectUtil.injectMock(commandClient, "webClient", webClient);
+
+        command.execute(commandClient);
+
+        assertEquals(commandClient.getRegistry().get("testvar"), "testchallenge");
+        assertEquals(commandClient.getRegistry().get("testimage"), "testcode");
     }
 }
